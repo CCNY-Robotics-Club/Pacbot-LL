48d54eb98820e9a758fb4240b27b3d5a4d35115b
Benjamin <47288387+nimajneben@users.noreply.github.com>
2024-02-29 21:53:43 -0500

    add motor drive code

---
+++ src/motor.cpp
@@ -0,0 +1,51 @@
+#include <Arduino.h>
+#include "motor.h"
+/**
+ * Instantiate a Motor Object.
+ * @param pinL pin connected to left drive
+ * @param pinR pin connected to right drive
+*/
+motor::motor(int pinL, int pinR)
+{
+    // Initialize Pins
+    this->pinL = pinL;
+    this->pinR = pinR;
+}
+
+/**
+ * Drive motor counterclockwise (left)
+ * @param speed motor speed
+ * TODO: range?
+*/
+void motor::driveL(int speed)
+{
+    this->stop();
+    this->speed = speed;
+    if (speed < 200) analogWriteFreq(500);
+    else analogWriteFreq(500);
+    analogWrite(pinL,speed);
+
+}
+
+/**
+ * Drive motor clockwise (right)
+ * @param speed motor speed
+ * TODO: range?
+*/
+void motor::driveR(int speed)
+{
+    this->stop();
+    this->speed = speed;
+    if (speed < 200) analogWriteFreq(500);
+    else analogWriteFreq(500);
+    analogWrite(pinR,speed);
+}
+
+/**
+ * Halt the motor.
+*/
+void motor::stop()
+{
+    analogWrite(pinL,0);
+    analogWrite(pinR,0);
+}
