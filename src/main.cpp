e52d3d656e3ee09ef2b18ce109874204087a3fe8
Benjamin <47288387+nimajneben@users.noreply.github.com>
2024-02-29 21:53:50 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
+#include "motor.h"
 #include <Arduino.h>
 
+motor m1(10,11);
 void setup() {
   // put your setup code here, to run once:
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  analogWrite(11,255);
+  m1.driveL(255);
+  delay(500);
+  m1.driveR(255);
+  delay(500);
 }
