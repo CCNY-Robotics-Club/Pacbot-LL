ae3debd95f95f2e9a7280a0e28a6398675a01c26
nimajneben <47288387+nimajneben@users.noreply.github.com>
2024-03-02 22:51:31 -0500

    Update motor test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
-#include "motor.h"
 #include <Arduino.h>
+#include <FreeRTOS.h>
+#include "motor.h"
 
 motor m1(10,11);
 void setup() {
@@ -8,8 +9,8 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  m1.drive(100);
-  delay(500);
-  m1.drive(-100);
-  delay(500);
+  m1.drive(50,5000,1);
+  delay(5000);
+  m1.drive(255,5000,1);
+  delay(5000);
 }
