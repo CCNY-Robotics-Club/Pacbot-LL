3f81bae8c08dd2332a55c45e30ce4659648ba2f1
nimajneben <47288387+nimajneben@users.noreply.github.com>
2024-03-03 22:05:46 -0500

    update motor test code
    tests the new drive system!

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,14 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  for (int i = 0; i < 255; i+=5)
+  {
+    m1.drive(i,800,1);
+    delay(2000);
+  }
   m1.drive(50,5000,1);
-  delay(5000);
-  m1.drive(255,5000,1);
+  delay(1000);
+  m1.drive(0,5000,1);
   delay(5000);
 }
